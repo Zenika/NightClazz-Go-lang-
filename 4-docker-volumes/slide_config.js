dc4dc018aae124ca7672f64529c033ef3959df35
Vincent Demeester <vincent@sbr.pm>
2016-02-03 23:59:04 +0100

    Add 4-docker-volumes init

---
+++ 4-docker-volumes/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Volumes docker', 
+     subtitle: 'Nightclazz', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/golang.ico', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'vdemeester', 
+     company: 'Zenika', 
+     gplus: '', 
+     twitter: 'ZenikaIT', 
+     www: 'http://www.zenika.com', 
+     github: 'http://github.com/zenika', 
+   }]
+};
